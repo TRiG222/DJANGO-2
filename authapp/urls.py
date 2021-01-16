e82f87ace3cc1321a0a53d611e2a008c72810537
TRiG <gerysh07@gmail.com>
2021-01-16 20:05:54 +0300

    LESSON 1

---
+++ authapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, re_path
+
+
+import authapp.views as authapp
+
+app_name = 'authapp'
+
+urlpatterns = [
+    path('login/', authapp.login, name='login'),
+    path('register/', authapp.register, name='register'),
+    path('profile/', authapp.profile, name='profile'),
+    path('logout/', authapp.logout, name='logout'),
+    re_path(r'^verify/(?P<email>.+)/(?P<activation_key>\w+)/$', authapp.verify, name='verify')
+]
